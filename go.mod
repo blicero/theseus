5c85d13ae22bd66e1ac820c42f50a8120a887929
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-30 21:28:27 +0200

    Add packages common and logdomain, init go module manifesto

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/blicero/theseus
+
+go 1.18
+
+require (
+	github.com/blicero/krylib v0.0.0-20210821183726-381c76f977eb
+	github.com/hashicorp/logutils v1.0.0
+	github.com/odeke-em/go-uuid v0.0.0-20151221120446-b211d769a9aa
+)
---
+++ logdomain/logdomain.go
@@ -0,0 +1,31 @@
+// /home/krylon/go/src/github.com/blicero/raconteur/logdomain/logdomain.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 06. 09. 2021 by Benjamin Walkenhorst
+// (c) 2021 Benjamin Walkenhorst
+// Time-stamp: <2022-06-30 21:26:38 krylon>
+
+// Package logdomain provides constants for log sources.
+package logdomain
+
+//go:generate stringer -type=ID
+
+// ID represents a log source
+type ID uint8
+
+// These constants signify the various parts of the application.
+const (
+	Common ID = iota
+	DBPool
+	Database
+	GUI
+)
+
+// AllDomains returns a slice of all the known log sources.
+func AllDomains() []ID {
+	return []ID{
+		Common,
+		DBPool,
+		Database,
+		GUI,
+	}
+} // func AllDomains() []ID
