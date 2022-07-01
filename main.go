b535dcf5d5f3c98042ed734f01bb6d077f2896c9
Benjamin Walkenhorst <krylon@posteo.de>
2022-07-01 20:42:01 +0200

    Add main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+// /home/krylon/go/src/github.com/blicero/theseus/main.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 01. 07. 2022 by Benjamin Walkenhorst
+// (c) 2022 Benjamin Walkenhorst
+// Time-stamp: <2022-07-01 20:40:24 krylon>
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/blicero/theseus/common"
+)
+
+func main() {
+	fmt.Printf("%s %s -- IMPLEMENT ME!\n",
+		common.AppName,
+		common.Version)
+}
