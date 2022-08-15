cc4f61c53bb4e7097623489acbf985c577c5a406
Benjamin Walkenhorst <krylon@posteo.de>
2022-08-15 21:19:21 +0200

    Lazily working on the logreader for GNU/Linux systems

--- clients/logreader/reader/reader/reader_linux.go
+++ clients/logreader/reader/reader/reader_linux.go
@@ -2,12 +2,20 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 27. 07. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-08-12 21:54:46 krylon>
+// Time-stamp: <2022-08-13 20:41:26 krylon>
 
+// Package reader provides Reader struct which knows how to read the log
+// files on various Unix systems.
 package reader
 
-import "log"
+import (
+	"log"
+	"regexp"
+)
 
+// Reader bla
 type Reader struct {
 	log *log.Logger
+	pat []*regexp.Regexp
+	srv string
 }
