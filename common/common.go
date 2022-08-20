d187a89fdcbacb45e6ddca115b33a23e45cde040
Benjamin Walkenhorst <krylon@posteo.de>
2022-08-20 18:46:41 +0200

    Bump version number to 0.2.0

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 06. 09. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2022-07-25 18:22:12 krylon>
+// Time-stamp: <2022-08-20 18:43:56 krylon>
 
 package common
 
@@ -34,7 +34,7 @@ import (
 // application.
 const (
 	AppName                  = "Theseus"
-	Version                  = "0.1.0"
+	Version                  = "0.2.0"
 	Debug                    = true
 	TimestampFormatMinute    = "2006-01-02 15:04"
 	TimestampFormat          = "2006-01-02 15:04:05"
