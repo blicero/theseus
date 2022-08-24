e230a98e68325596898ac62be8335f57065b9f95
Benjamin Walkenhorst <krylon@posteo.de>
2022-08-24 21:15:56 +0200

    Backend: Minor fix to shut up the linter

--- backend/helpers.go
+++ backend/helpers.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 19. 07. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-07-19 00:01:23 krylon>
+// Time-stamp: <2022-08-24 21:10:32 krylon>
 
 package backend
 
@@ -11,7 +11,7 @@ import "time"
 func durAbs(d time.Duration) time.Duration {
 	if d < 0 {
 		return -d
-	} else {
-		return d
 	}
+
+	return d
 } // func durAbs(d time.Duration) time.Duration
