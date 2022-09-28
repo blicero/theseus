ffa186ac27150f3a16d45d06a8089c8840764ede
Benjamin Walkenhorst <krylon@posteo.de>
2022-09-28 20:55:25 +0200

    UI: Context menu shows up, doesn't do anything, yet

--- ui/contextmenu.go
+++ ui/contextmenu.go
@@ -2,11 +2,14 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 28. 09. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-09-28 18:50:31 krylon>
+// Time-stamp: <2022-09-28 20:46:40 krylon>
 
 package ui
 
 import (
+	"fmt"
+
+	"github.com/blicero/theseus/objects"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
@@ -21,6 +24,7 @@ func (g *GUI) handleReminderClick(view *gtk.TreeView, evt *gdk.Event) {
 
 	var (
 		err    error
+		msg    string
 		exists bool
 		x, y   float64
 		path   *gtk.TreePath
@@ -93,4 +97,79 @@ func (g *GUI) handleReminderClick(view *gtk.TreeView, evt *gdk.Event) {
 
 	g.log.Printf("[DEBUG] ID of clicked-on row is %d\n",
 		id)
+
+	var (
+		r    objects.Reminder
+		menu *gtk.Menu
+	)
+
+	if r, exists = g.reminders[id]; !exists {
+		msg = fmt.Sprintf("Cannot find Reminder #%d",
+			id)
+		g.log.Printf("[ERROR] %s\n",
+			msg)
+		g.pushMsg(msg)
+		return
+	} else if menu, err = g.mkReminderContextMenu(path, &r); err != nil {
+		msg = fmt.Sprintf("Cannot create context menu for Reminder %q: %s",
+			r.Title,
+			err.Error())
+		g.log.Printf("[ERROR] %s\n",
+			msg)
+		g.pushMsg(msg)
+		return
+	}
+
+	menu.ShowAll()
+	menu.PopupAtPointer(evt)
 } // func (g *GUI) handleReminderClick(view *gtk.TreeView, evt *gdk.Event)
+
+func (g *GUI) mkReminderContextMenu(path *gtk.TreePath, r *objects.Reminder) (*gtk.Menu, error) {
+	var (
+		err               error
+		msg               string
+		editItem, delItem *gtk.MenuItem
+		toggleItem        *gtk.CheckMenuItem
+		menu              *gtk.Menu
+	)
+
+	if menu, err = gtk.MenuNew(); err != nil {
+		msg = fmt.Sprintf("Cannot create Menu: %s",
+			err.Error())
+		goto ERROR
+	} else if editItem, err = gtk.MenuItemNewWithMnemonic("_Edit"); err != nil {
+		msg = fmt.Sprintf("Cannot create menu item Edit: %s",
+			err.Error())
+		goto ERROR
+	} else if delItem, err = gtk.MenuItemNewWithMnemonic("_Delete"); err != nil {
+		msg = fmt.Sprintf("Cannot create menu item Delete: %s",
+			err.Error())
+		goto ERROR
+	} else if toggleItem, err = gtk.CheckMenuItemNewWithLabel("_Active"); err != nil {
+		msg = fmt.Sprintf("Cannot create menu item Active: %s",
+			err.Error())
+		goto ERROR
+	}
+
+	// Set up signal handlers!
+
+	menu.Append(editItem)
+	menu.Append(delItem)
+	menu.Append(toggleItem)
+
+	return menu, nil
+ERROR:
+	g.log.Printf("[ERROR] %s\n", msg)
+	g.pushMsg(msg)
+	g.displayMsg(msg)
+	return nil, err
+} // func (g *GUI) mkReminderContextMenu(path *gtk.TreePath, r *objects.Reminder) (*gtk.Menu, error)
+
+// func (g *GUI) handleReminderClickEdit() {
+// 	var (
+// 		err error
+// 		msg string
+
+// 	)
+// }
+// func (g *GUI) handleReminderClickEdit()
